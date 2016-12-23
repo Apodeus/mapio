f00aa4949ff3771bc11bc91ef136d1785321f58a
Echoffee <light.runa@gmail.com>
2016-12-23 19:45:24 +0100

    Add first part of map_save : get tiles positions & types

--- Projet/src/mapio.c
+++ Projet/src/mapio.c
@@ -9,6 +9,18 @@
 
 #ifdef PADAWAN
 
+struct s_map_case
+{
+  int x;
+  int y;
+  int type;
+
+  struct s_map_case* next;
+};
+
+typedef struct s_map_case* map_case;
+
+
 void map_new (unsigned width, unsigned height)
 {
   map_allocate (width, height);
@@ -39,6 +51,41 @@ void map_new (unsigned width, unsigned height)
 void map_save (char *filename)
 {
   // TODO
+  FILE* f = fopen(filename, "w");
+  int num_items = map_objects();
+  fprintf(f, "%d\n", num_items);
+  int w = map_width();
+  int h = map_height();
+  map_case prev = NULL;
+  map_case first = prev;
+  map_case very_first = first;
+  for (int i = 0; i < w; i++)
+    for (int j = 0; j < h; j++)
+    {
+      int o = map_get(i, j);
+      if (o != MAP_OBJECT_NONE)
+      {
+        map_case m = (map_case)malloc(sizeof(struct s_map_case));
+        m->x = i;
+        m->y = j;
+        m->type = o;
+
+        if (prev == NULL)
+          first = m;
+        else
+          prev->next = m;
+        prev = m;
+      }
+    }
+
+  while (first->next != NULL)
+    {
+      fprintf(f, "%d %d %d\n", first->x, first->y, first->type);
+      first = first->next;
+    }
+
+
+  fclose(f);
   fprintf (stderr, "Sorry: Map save is not yet implemented\n");
 }
 
