bcca6b2a9d8943bbf9fea625c9af6f6fab38c0c2
Echoffee <light.runa@gmail.com>
2017-01-03 15:04:57 +0100

    Add daemon stuff, does not compile

--- Projet/src/tempo.c
+++ Projet/src/tempo.c
@@ -12,28 +12,42 @@
 // Return number of elapsed µsec since... a long time ago
 static unsigned long get_time (void)
 {
-  struct timeval tv;
+	struct timeval tv;
 
-  gettimeofday (&tv ,NULL);
+	gettimeofday (&tv ,NULL);
 
   // Only count seconds since beginning of 2016 (not jan 1st, 1970)
-  tv.tv_sec -= 3600UL * 24 * 365 * 46;
-  
-  return tv.tv_sec * 1000000UL + tv.tv_usec;
+	tv.tv_sec -= 3600UL * 24 * 365 * 46;
+
+	return tv.tv_sec * 1000000UL + tv.tv_usec;
 }
 
 #ifdef PADAWAN
 
+void* daemon()
+{
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set, SIGALRM);
+	while(1)
+	{
+		fprintf(stderr, "Running...\n");
+		sigsuspend(&set);
+		fprintf(stderr, "___");
+	}
+}
+
 // timer_init returns 1 if timers are fully implemented, 0 otherwise
 int timer_init (void)
 {
-  // TODO
-  return 0; // Implementation not ready
+	pthread_t d;
+	pthread_create(&d, NULL, daemon, NULL);
+	return 0; // Implementation not ready
 }
 
 void timer_set (Uint32 delay, void *param)
 {
-  // TODO
+	// TODO
 }
 
 #endif
